7afa1ddf8f60a606982d16022ca47a5a7ad5a78f
Simple1414 <rajabovid@gmail.com>
2026-06-02 20:12:23 +0500

    Add form submission handling and alert message

--- js_script_Version2.js
+++ js_script_Version2.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Сайт омода аст барои истифода!');
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  alert("Паёми шумо қабул шуд. Ташаккур!");
+
+  form.reset();
 });
